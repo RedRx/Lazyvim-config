9fcd953206a388927c6e18023a22976c90c3b462
BEARYDEV <teerapat.tech@gmail.com>
2024-03-21 14:33:09 +0700

    update flash nvim

--- plugins/editor.lua
+++ plugins/editor.lua
@@ -1,7 +1,8 @@
 return {
   {
-    -- enabled = false,
     "folke/flash.nvim",
+    event = "VeryLazy",
+    vscode = true,
     ---@type Flash.Config
     opts = {
       search = {
@@ -9,6 +10,40 @@ return {
         multi_window = false,
         wrap = false,
         incremental = true,
+      },
+    },
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump()
+        end,
+        desc = "Flash",
+      },
+      {
+        "S",
+        mode = { "n", "o", "x" },
+        function()
+          require("flash").treesitter()
+        end,
+        desc = "Flash Treesitter",
+      },
+      {
+        "r",
+        mode = "o",
+        function()
+          require("flash").remote()
+        end,
+        desc = "Remote Flash",
+      },
+      {
+        "R",
+        mode = { "o", "x" },
+        function()
+          require("flash").treesitter_search()
+        end,
+        desc = "Treesitter Search",
       },
     },
   },
