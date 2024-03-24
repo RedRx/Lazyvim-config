347e0ec78049f5195f41192f057f142236fddac4
BEARYDEV <teerapat.tech@gmail.com>
2024-03-24 22:10:16 +0700

    update

--- config/lazy.lua
+++ config/lazy.lua
@@ -14,6 +14,7 @@ require("lazy").setup({
       import = "lazyvim.plugins",
       opts = {
         colorscheme = "solarized-osaka",
+        -- colorscheme = "catppuccin-mocha",
         news = {
           lazyvim = true,
           neovim = true,
